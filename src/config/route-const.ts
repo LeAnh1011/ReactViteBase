09fbee602ddf842afa6a1bfc5e29d14dca2011f7
leanh1011 <leanh2194@gmail.com>
2023-09-07 17:58:46 +0700

    fix route

--- src/config/route-const.ts
+++ src/config/route-const.ts
@@ -3,9 +3,9 @@ import { join } from "path";
 
 // Portal Route:
 export const PORTAL_ROUTE = "/portal";
-export const PORTAL_APP_CATALOG_ROUTE = join(PORTAL_ROUTE, "/app-catalog");
+export const PORTAL_APP_CATALOG_ROUTE = "/app-catalog";
+export const PORTAL_LANDING_PAGE_ROUTE = "/landing-page";
 export const PORTAL_APP_USER_ROUTE = join(PORTAL_ROUTE, "/app-user");
-export const PORTAL_LANDING_PAGE_ROUTE = join(PORTAL_ROUTE, "/landing-page");
 
 // Dashbroad Route:
 export const DASHBROAD_ROUTE = join(ROOT_ROUTE, "/dashbroad");
